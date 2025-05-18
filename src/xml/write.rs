779577dc1154c8e34a81f7dd4082aa0a5646392e
Ame <ajiiisai@protonmail.com>
2025-05-18 17:54:35 +0200

    refactor: improve performance and general rewrite

--- src/xml/write.rs
+++ src/xml/write.rs
@@ -1,15 +1,13 @@
 use crate::elements::{Document, FontType, Paragraph, Run, RunProperties};
 use crate::errors::RudocxError;
 
-use quick_xml::events::BytesText;
 use quick_xml::Writer;
+use quick_xml::events::BytesText;
 use std::io::Cursor;
 
+type XmlWriter = Writer<Cursor<Vec<u8>>>;
+
 pub fn generate(document: &Document) -> Result<String, RudocxError> {
-    write_document_xml(document)
-}
-
-fn write_document_xml(document: &Document) -> Result<String, RudocxError> {
     let mut writer = Writer::new(Cursor::new(Vec::new()));
 
     writer
@@ -18,208 +16,119 @@ fn write_document_xml(document: &Document) -> Result<String, RudocxError> {
             "xmlns:w",
             "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
         ))
-        .write_inner_content(|writer| {
-            write_body_xml(writer, document)?;
-            Ok(())
-        })?;
+        .write_inner_content(|writer| write_body(writer, document))
+        .map_err(|e| RudocxError::XmlError(e.into()))?;
 
     let xml_bytes = writer.into_inner().into_inner();
     String::from_utf8(xml_bytes).map_err(RudocxError::Utf8Error)
 }
 
-fn write_body_xml(
-    writer: &mut Writer<Cursor<Vec<u8>>>,
-    document: &Document,
-) -> Result<(), RudocxError> {
-    writer
-        .create_element("w:body")
-        .write_inner_content(|writer| {
-            for p in &document.paragraphs {
-                write_paragraph_xml(writer, p)?;
-            }
-            Ok(())
-        })?;
-    Ok(())
-}
-
-fn write_paragraph_xml(
-    writer: &mut Writer<Cursor<Vec<u8>>>,
-    paragraph: &Paragraph,
-) -> Result<(), RudocxError> {
-    writer.create_element("w:p").write_inner_content(|writer| {
-        for r in &paragraph.runs {
-            write_run_xml(writer, r)?;
+fn write_body(writer: &mut XmlWriter, document: &Document) -> std::io::Result<()> {
+    let element = writer.create_element("w:body");
+    element.write_inner_content(|writer| {
+        for paragraph in &document.paragraphs {
+            write_paragraph(writer, paragraph)?;
         }
         Ok(())
     })?;
     Ok(())
 }
 
-fn write_run_xml(writer: &mut Writer<Cursor<Vec<u8>>>, run: &Run) -> Result<(), RudocxError> {
-    writer.create_element("w:r").write_inner_content(|writer| {
-        write_run_properties_xml(writer, &run.properties)?;
-        if run.space_preserve {
-            writer
-                .create_element("w:t")
-                .write_text_content(BytesText::new(&run.text))?;
+fn write_paragraph(writer: &mut XmlWriter, paragraph: &Paragraph) -> std::io::Result<()> {
+    let element = writer.create_element("w:p");
+    element.write_inner_content(|writer| {
+        for run in &paragraph.runs {
+            write_run(writer, run)?;
+        }
+        Ok(())
+    })?;
+    Ok(())
+}
+
+fn write_run(writer: &mut XmlWriter, run: &Run) -> std::io::Result<()> {
+    let element = writer.create_element("w:r");
+    element.write_inner_content(|writer| {
+        if run.properties.has_formatting() {
+            write_run_properties(writer, &run.properties)?;
+        }
+
+        if !run.space_preserve {
+            let text_element = writer.create_element("w:t");
+            text_element.write_text_content(BytesText::new(&run.text))?;
         } else {
-            writer
-                .create_element("w:t")
-                .with_attribute(("xml:space", "preserve"))
-                .write_text_content(BytesText::new(&run.text))?; //TODO parse breaklines
+            let mut text_element = writer.create_element("w:t");
+            text_element = text_element.with_attribute(("xml:space", "preserve"));
+            text_element.write_text_content(BytesText::new(&run.text))?;
         }
+
         Ok(())
     })?;
     Ok(())
 }
 
-fn write_run_properties_xml(
-    writer: &mut Writer<Cursor<Vec<u8>>>,
-    properties: &RunProperties,
-) -> Result<(), RudocxError> {
-    if properties.has_formatting() {
-        writer
-            .create_element("w:rPr")
-            .write_inner_content(|writer| {
-                // Start run properties
+fn write_run_properties(writer: &mut XmlWriter, properties: &RunProperties) -> std::io::Result<()> {
+    let element = writer.create_element("w:rPr");
+    element.write_inner_content(|writer| {
+        for (condition, element_name) in [
+            (properties.bold, "w:b"),
+            (properties.italic, "w:i"),
+            (properties.strike, "w:strike"),
+            (properties.dstrike, "w:dstrike"),
+        ] {
+            if condition {
+                writer.create_element(element_name).write_empty()?;
+            }
+        }
 
-                if properties.bold {
-                    writer.create_element("w:b").write_empty()?;
-                }
+        if let Some(underline) = &properties.underline {
+            write_attribute_element(writer, "w:u", "w:val", &underline.value())?;
+        }
 
-                if properties.italic {
-                    writer.create_element("w:i").write_empty()?;
-                }
+        if let Some(color) = &properties.color {
+            write_attribute_element(writer, "w:color", "w:val", &color.value())?;
+        }
 
-                if let Some(underline) = &properties.underline {
-                    writer
-                        .create_element("w:u")
-                        .with_attribute(("w:val", underline.value().as_str()))
-                        .write_empty()?;
-                }
+        if let Some(size) = &properties.size {
+            write_attribute_element(writer, "w:sz", "w:val", &size.to_string())?;
+        }
 
-                if let Some(color) = &properties.color {
-                    writer
-                        .create_element("w:color")
-                        .with_attribute(("w:val", color.value().as_str()))
-                        .write_empty()?;
-                }
-
-                if let Some(size) = &properties.size {
-                    writer
-                        .create_element("w:sz")
-                        .with_attribute(("w:val", size.to_string().as_str()))
-                        .write_empty()?;
-                }
-
-                //TODO: Handle multiple types. Right now only the hint is written.
-                if let Some(font_set) = &properties.font {
-                    match font_set.get_hint_value() {
-                        FontType::Default => (),
-                        _ => {
-                            let attr = &format!("w:{}", font_set.get_hint_value().to_string());
-                            if let Ok(val) = font_set.get_hint() {
-                                writer
-                                    .create_element("w:rFonts")
-                                    .with_attribute((attr.as_str(), val.as_str()))
-                                    .write_empty()?;
-                            }
-                        }
+        if let Some(font_set) = &properties.font {
+            match font_set.get_hint_value() {
+                FontType::Default => (),
+                _ => {
+                    if let Ok(val) = font_set.get_hint() {
+                        let attr = format!("w:{}", font_set.get_hint_value());
+                        write_attribute_element(writer, "w:rFonts", &attr, &val)?;
                     }
                 }
+            }
+        }
 
-                if let Some(highlight) = &properties.highlight {
-                    writer
-                        .create_element("w:highlight")
-                        .with_attribute(("w:val", highlight.value().as_str()))
-                        .write_empty()?;
-                }
+        if let Some(highlight) = &properties.highlight {
+            write_attribute_element(writer, "w:highlight", "w:val", &highlight.value())?;
+        }
 
-                if properties.strike {
-                    writer.create_element("w:strike").write_empty()?;
-                }
+        if let Some(valign) = &properties.valign {
+            write_attribute_element(writer, "w:vertAlign", "w:val", &valign.value())?;
+        }
 
-                if properties.dstrike {
-                    writer.create_element("w:dstrike").write_empty()?;
-                }
+        if let Some(spacing) = &properties.spacing {
+            write_attribute_element(writer, "w:spacing", "w:val", &spacing.to_string())?;
+        }
 
-                if let Some(valign) = &properties.valign {
-                    writer
-                        .create_element("w:vertAlign")
-                        .with_attribute(("w:val", valign.value().as_str()))
-                        .write_empty()?;
-                }
-
-                if let Some(spacing) = &properties.spacing {
-                    writer
-                        .create_element("w:spacing")
-                        .with_attribute(("w:val", spacing.to_string().as_str()))
-                        .write_empty()?;
-                }
-
-                // End of run properties
-                Ok(())
-            })?;
-    }
+        Ok(())
+    })?;
     Ok(())
 }
 
-// ---
-// Helper function to generate the word/document.xml content
-#[deprecated]
-pub fn generate_document_xml(document: &Document) -> Result<String, RudocxError> {
-    let mut writer = Writer::new(Cursor::new(Vec::new()));
-
-    writer
-        .create_element("w:document")
-        .with_attribute((
-            "xmlns:w",
-            "http://schemas.openxmlformats.org/wordprocessingml/2006/main",
-        ))
-        .write_inner_content(|writer| {
-            writer
-                .create_element("w:body")
-                .write_inner_content(|writer| {
-                    for p in &document.paragraphs {
-                        writer.create_element("w:p").write_inner_content(|writer| {
-                            for r in &p.runs {
-                                writer.create_element("w:r").write_inner_content(|writer| {
-                                    if r.properties.has_formatting() {
-                                        writer.create_element("w:rPr").write_inner_content(
-                                            |writer| {
-                                                if r.properties.bold {
-                                                    writer.create_element("w:b").write_empty()?;
-                                                }
-                                                if r.properties.italic {
-                                                    writer.create_element("w:i").write_empty()?;
-                                                }
-                                                if let Some(color) = &r.properties.color {
-                                                    writer
-                                                        .create_element("w:color")
-                                                        .with_attribute((
-                                                            "w:val",
-                                                            color.value().as_str(),
-                                                        ))
-                                                        .write_empty()?;
-                                                }
-                                                Ok(())
-                                            },
-                                        )?;
-                                    }
-                                    writer
-                                        .create_element("w:t")
-                                        .write_text_content(BytesText::new(&r.text))?;
-                                    Ok(())
-                                })?;
-                            }
-                            Ok(())
-                        })?;
-                    }
-                    Ok(())
-                })?;
-            Ok(())
-        })?;
-
-    let xml_bytes = writer.into_inner().into_inner();
-    String::from_utf8(xml_bytes).map_err(RudocxError::Utf8Error)
+fn write_attribute_element(
+    writer: &mut XmlWriter,
+    element: &str,
+    attr_name: &str,
+    attr_value: &str,
+) -> std::io::Result<()> {
+    let mut el = writer.create_element(element);
+    el = el.with_attribute((attr_name, attr_value));
+    el.write_empty()?;
+    Ok(())
 }
